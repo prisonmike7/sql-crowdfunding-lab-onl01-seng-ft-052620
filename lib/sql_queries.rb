28f29c3e69167e8c972160085b860026fe918c1c
Michael Harris <mwharris7@gmail.com>
2020-06-30 06:49:42 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
   <<-SQL
-    SELECT user.name, user.age, SUM(pledges.amount)
+    SELECT users.name, users.age, SUM(pledges.amount)
     FROM pledges
     JOIN users
     ON pledges.user_id = users.id
