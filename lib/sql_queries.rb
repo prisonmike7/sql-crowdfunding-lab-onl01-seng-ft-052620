df728812c680003ec87237d1c2cd3e55e1b56299
Michael Harris <mwharris7@gmail.com>
2020-06-30 06:57:28 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -30,7 +30,7 @@ end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   <<-SQL
-    SELECT projects.title, 
+    SELECT projects.title,
     SUM(pledges.amount) - projects.funding_goal AS amount_over_goal
     FROM projects
     JOIN pledges
